579d96355f16ac8a9d3fb6de59a65ff110b7f4a0
emoltz <es5541@nyu.edu>
2023-07-08 18:20:27 -0400

    basic stuff working!

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/components/ui/button";
 
 export function NavBar() {
     return (
-        <nav className={"flex items-center justify-between flex-wrap bg-gray-400 p-6"}>
+        <nav className={"flex items-center justify-between flex-wrap bg-black p-6"}>
             <div className={"flex items-center flex-shrink-0 text-white mr-6"}>
                 <span className={"font-semibold text-xl tracking-right"}>Teacher's Toolkit</span>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,54 @@
 "use client";
 import {Button} from '@/components/ui/button';
 import {Textarea} from '@/components/ui/textarea';
+import {ChangeEvent, useState} from "react";
 
 export default function Home() {
+    const [text, setText] = useState<string>('');
+    const [response, setResponse] = useState<string>('');
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value)
+
+    }
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        const language = "English";
+
+        const response = await fetch('/api/openai', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+
+            },
+            body: JSON.stringify({
+                gradeLevel: "1st Grade",
+                language: language,
+                prompt: text,
+            }),
+        });
+        const data = await response.json();
+        setResponse(data.aiResponse);
+        console.log(data);
+    }
+
+
     return (
         <>
             <div className="grid w-full gap-2">
-                <Textarea placeholder="Type your message here."/>
-                <Button>Send message</Button>
+                <Textarea
+                    style={{height: '200px'}}
+                    placeholder="Type your message here."
+                    onChange={handleChange}
+                />
             </div>
+            <Button
+                onClick={handleSubmit}
+            >Send message</Button>
+
+            <div>
+                {response}
+            </div>
+
         </>
 
     )
