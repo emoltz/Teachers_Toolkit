360fb4b07683895f7010136afb58e3e7089941cc
emoltz <es5541@nyu.edu>
2023-07-13 18:01:52 -0400

    navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,15 +4,21 @@ import Link from "next/link";
 import {useCurrentUser} from "@/lib/hooks";
 import {Icons} from "@/components/ui/icons";
 import * as React from "react";
-import {UserCircle} from "@phosphor-icons/react";
+import {useState} from "react";
+import {List, UserCircle} from "@phosphor-icons/react";
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTrigger,} from "@/components/ui/sheet";
+
 
 export function NavBar() {
     const {user, loading} = useCurrentUser();
+    const [isOpen, setIsOpen] = useState(false);
+
     const routes = [
         {
             name: "About",
             path: "/about"
         },
+
     ]
 
     return (
@@ -30,38 +36,86 @@ export function NavBar() {
                 </span>
             </div>
             <span className={"flex-grow"}/>
-            {routes.map((route, index) => (
-                <div
-                    key={index}
-                >
-                    <Button className={"text-white"}>
-                        <Link href={route.path}>
-                            {route.name}
-                        </Link>
-                    </Button>
+            <div className={"flex"}>
+                <div className={"block lg:hidden md:hidden sm:hidden"}>
+                    {/*Hamburger*/}
+                    <Sheet>
+                        <SheetTrigger>
+                            <div onClick={() => setIsOpen(!isOpen)}
+                                 className={"flex items-center px-3 py-2 text-white rounded hover:text-white hover:border-white"}
+                            >
+                                <List size={25}/>
+
+                            </div>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                Links
+                            </SheetHeader>
+                            <SheetDescription>
+
+                            </SheetDescription>
+                            {routes.map((route, index) => (
+                                <div
+                                    key={index}>
+
+                                    <Link
+                                        onClick={close}
+
+                                        href={route.path}>
+                                        {route.name}
+                                    </Link>
+                                </div>
+                            ))}
+                            <div>
+
+                                <Link
+                                    href={"/login"}
+                                >
+                                    Profile/Login
+                                </Link>
+                            </div>
+
+
+                        </SheetContent>
+                    </Sheet>
                 </div>
-            ))}
+                <div className={"sm:block hidden"}>
 
-            <div className={""}>
-                <Link
-                    href={"/login"}
-                >
+                    {routes.map((route, index) => (
+                        <div
+                            key={index}
+                        >
+                            <Button className={"text-white"}>
+                                <Link href={route.path}>
+                                    {route.name}
+                                </Link>
+                            </Button>
+                        </div>
+                    ))}
+                </div>
 
-                    <Button id={"menuButton"}
-                            variant={"outline"}
-                            disabled={loading}
+                <div className={"sm:block hidden"}>
+                    <Link
+                        href={"/login"}
                     >
-                        {loading ? (
-                                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
-                                ) : (
-                                    <UserCircle size={25}/>
-                                )}{" "}
-                        {user ? "My Stuff" : "Login"}
 
-                    </Button>
-                </Link>
+                        <Button id={"menuButton"}
+                                variant={"outline"}
+                                disabled={loading}
+                        >
+                            {loading ? (
+                                <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
+                            ) : (
+                                <UserCircle size={25}/>
+                            )}
+                            {user ? "My Stuff" : "Login"}
+
+                        </Button>
+                    </Link>
+                </div>
+
             </div>
-
 
         </nav>
     )
