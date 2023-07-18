09a8bed9f6e24ec2db9d93dcee870c97c4058f82
emoltz <es5541@nyu.edu>
2023-07-18 17:38:21 -0400

    bug fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,36 +1,13 @@
-"use client"
 import LogOut from "@/app/profile/LogOut";
 import {tailwindStyles} from "@/lib/styles";
-import {useCurrentUser} from "@/lib/hooks";
-import Loading from "@/components/ui/Loading";
-
-export async function getServersideProps(){
-    const {user, loading} = useCurrentUser();
-    if (!user){
-        return{
-            redirect: {
-                destination: "/login",
-                permanent: false,
-            }
-        }
-    }
-    return {
-        props: {}
-    }
-}
 
 export default function page() {
-    const {user, loading} = useCurrentUser();
-
-    if (loading){
-        return <Loading/>
-    }
 
     return (
 
         <div>
             <div className={tailwindStyles.heading1}>
-                Welcome, {user?.displayName}
+                My Profile
             </div>
 
             <LogOut/>
